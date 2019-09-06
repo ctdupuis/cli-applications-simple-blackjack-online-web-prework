691353769d6f7cdd93cca033b40102daa946f287
Cody Dupuis <ctdupuis7@gmail.com>
2019-09-06 04:21:50 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,7 +36,7 @@ def hit?(card_total)
 end
 
 def invalid_command
-  print "Please enter a valid command"
+  puts "Please enter a valid command"
 end
 
 #####################################################
